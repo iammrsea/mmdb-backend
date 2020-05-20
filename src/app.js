aaf85af76bae1a2a60bde44ada9acf2930c57267
Ekwugha Andrew <ekwughaandrew@gmail.com>
2020-05-20 11:18:55 +0100

    initialized express app and other modules in app.js file

--- src/app.js
+++ src/app.js
@@ -1,27 +1,35 @@
-// const express = require('express');
-// const mongoose = require('mongoose');
-// const bodyParser = require('body-parser');
-// const dotEnv = require('dotenv');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const dotEnv = require('dotenv');
+const cors = require('cors');
 
-// //Creates express app
-// const app = express();
+//import routes handler
+const router = require('./modules/router/router.service');
 
-// //Initialize middleware
-// dotEnv.config();
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
+//Creates express app
+const app = express();
 
-// //Connect to database
-// mongoose
-// 	.connect(process.env.DB_HOST, { useNewUrlParser: true, useUnifiedTopology: true })
-// 	.then((value) => {
-// 		console.log(`Successfully connected to database: ${process.env.DB_HOST}`);
+//Initialize middleware
+dotEnv.config();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-// 		return app.listen(process.env.PORT || 5000);
-// 	})
-// 	.then(() => {
-// 		console.log(`🚀 Server ready at http://localhost:${process.env.PORT || 5000}`);
-// 	})
-// 	.catch((error) => {
-// 		console.log(error);
-// 	});
+//Handle route requests
+app.use('/api', router);
+
+//Connect to database
+mongoose
+	.connect(process.env.DB_HOST, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then((value) => {
+		console.log(`Successfully connected to database: ${process.env.DB_HOST}`);
+
+		return app.listen(process.env.PORT || 5000);
+	})
+	.then(() => {
+		console.log(`🚀 Server ready at http://localhost:${process.env.PORT || 5000}`);
+	})
+	.catch((error) => {
+		console.log(error);
+	});
